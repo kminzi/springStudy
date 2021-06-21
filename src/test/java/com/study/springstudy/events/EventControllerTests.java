92a41a460c5abcacb4b732ef76b9d773a9a6d0e7
kminzi <minji9096@naver.com>
2021-06-21 17:19:11 +0900

    Add test description annotation

--- src/test/java/com/study/springstudy/events/EventControllerTests.java
+++ src/test/java/com/study/springstudy/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package com.study.springstudy.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.springstudy.common.TestDescription;
 import org.apache.tomcat.jni.Local;
 import org.hamcrest.Matchers;
 import org.junit.Test;
@@ -40,6 +41,7 @@ public class EventControllerTests {
 //    EventRepository eventRepository;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -73,6 +75,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -101,6 +104,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -112,6 +116,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
