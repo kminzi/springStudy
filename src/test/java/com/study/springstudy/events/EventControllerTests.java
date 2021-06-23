5e65594aad04eeedba9cdc840b9e2b5dfb0f791a
kminzi <minji9096@naver.com>
2021-06-23 14:32:56 +0900

    create parameterized tests

--- src/test/java/com/study/springstudy/events/EventControllerTests.java
+++ src/test/java/com/study/springstudy/events/EventControllerTests.java
@@ -2,16 +2,11 @@ package com.study.springstudy.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.study.springstudy.common.TestDescription;
-import org.apache.tomcat.jni.Local;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
--- src/test/java/com/study/springstudy/events/EventTest.java
+++ src/test/java/com/study/springstudy/events/EventTest.java
@@ -1,10 +1,15 @@
 package com.study.springstudy.events;
 
 import com.study.springstudy.common.TestDescription;
-import org.junit.jupiter.api.Test;
+import junitparams.Parameters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import junitparams.JUnitParamsRunner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
 
     @Test
@@ -50,6 +55,31 @@ public class EventTest {
     }
 
     @Test
+    @TestDescription("파라미터를 적용한 무료 적용 비지니스 로직 테스트")
+    @Parameters(method = "paramsForTestFree")
+    public void testFree2(int basePrice, int maxPrice, boolean isFree){
+        //given
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
+    private Object[] paramsForTestFree(){
+        return new Object[] {
+                new Object[] {0, 0, true},
+                new Object[] {100, 0, false},
+                new Object[] {0, 100, false},
+                new Object[] {100, 200, false}
+        };
+    }
+
+    @Test
     @TestDescription("오프라인 적용 비지니스 로직 테스트")
     public void testOffline(){
         //given
@@ -73,6 +103,26 @@ public class EventTest {
 
         //then
         assertThat(event.isOffline()).isFalse();
+        System.out.println(event.builder().toString());
+    }
+
+    @Test
+    @TestDescription("파라미터를 이용한 오프라인 적용 비지니스 로직 테스트")
+    @Parameters({
+            "kt, true",
+            ", false"
+    })
+    public void testOffline2(String location, boolean isOffline){
+        //given
+        Event event = Event.builder()
+                .location(location)
+                .build();
+
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isOffline()).isEqualTo(isOffline);
         System.out.println(event.builder().toString());
     }
 }
