400f55bcecd580b29c32acc85524d43e21029644
kminzi <minji9096@naver.com>
2021-07-08 15:56:03 +0900

    Add Fail Test for AccountService
    AccountService에 대한 존재하지 않는 user의 이메일로 조회하는 경우 fail이 발생하는지 테스트 추가

--- src/test/java/com/study/springstudy/accounts/AccountServiceTest.java
+++ src/test/java/com/study/springstudy/accounts/AccountServiceTest.java
@@ -1,12 +1,16 @@
 package com.study.springstudy.accounts;
 
 import com.study.springstudy.common.TestDescription;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,12 +18,16 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
     //service테스트이므로 controller를 사용할 이유가 없음. 따라서 basecontroller 상속 x
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -52,4 +60,37 @@ public class AccountServiceTest {
         assertThat(userDetails.getPassword()).isEqualTo(password);
         System.out.println(userDetails.getAuthorities());
     }
+
+    @Test(expected = UsernameNotFoundException.class)
+    @TestDescription("존재하지 않는 user의 이메일로 조회하는 경우 - expected 사용")
+    public void findByUsernameFail1(){
+        String email = "error";
+        accountService.loadUserByUsername(email);
+    }
+
+    @Test
+    @TestDescription("존재하지 않는 user의 이메일로 조회하는 경우 - try catch 사용")
+    public void findByUsernameFail2(){
+        String email = "error";
+        try {
+            accountService.loadUserByUsername(email);
+            fail("supposed to be failed");
+        }catch (UsernameNotFoundException e){
+            assertThat(e.getMessage()).containsSequence(email);
+        }
+    }
+
+    @Test
+    @TestDescription("존재하지 않는 user의 이메일로 조회하는 경우 - @rule ExpectedException 사용")
+    public void findByUsernameFail3(){
+        //Expected
+        //기대하는 예외를 먼저 써줘야 정상 동작함
+        String email = "error";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(email));
+
+        //when
+        accountService.loadUserByUsername(email);
+    }
+
 }
