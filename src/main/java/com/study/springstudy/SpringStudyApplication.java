0f2efc17269cad0151830f615445054ef52dcf69
kminzi <minji9096@naver.com>
2021-07-09 11:20:03 +0900

    Add Spring Security config
    WebSecurityConfigurerAdapter를 상속받아 Spring Security설정을 추가
    /docs/index.html과 static resource에 모든 사용자 접근이 가능하게 설정

--- src/main/java/com/study/springstudy/SpringStudyApplication.java
+++ src/main/java/com/study/springstudy/SpringStudyApplication.java
@@ -1,9 +1,7 @@
 package com.study.springstudy;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringStudyApplication {
@@ -11,10 +9,4 @@ public class SpringStudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringStudyApplication.class, args);
     }
-
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
-
 }
---
+++ src/main/java/com/study/springstudy/configs/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.study.springstudy.configs;
+
+import com.study.springstudy.accounts.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration //bean 설정파일
+@EnableWebSecurity //이 annotation으로 인해 spring boot가 제공하는 기본 spring security설정은 적용되지 않음
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Bean
+    //oauth token을 저장하는 저장소
+    public TokenStore tokenStore(){
+        return new InMemoryTokenStore();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    //authenticationManager 만들기
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(accountService)
+                .passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    //security filter 적용에 대한 설정
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers("/docs/index.html");
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations()); //정적리소스에 대한 적용 제거
+    }
+
+    @Override
+    //이건 spring security 안으로는 들어오고 그 안에서 걸러지는 방식(websecurity -> httpsecurity)
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/docs/index.html").anonymous()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).anonymous();
+    }
+}
